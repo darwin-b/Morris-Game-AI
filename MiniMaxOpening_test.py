68ed3013fb697a74b10c2cd341123b835908afb6
darwin bollepalli <darwin.bollepalli@gmail.com>
2021-07-13 14:37:36 -0500

    ignore: added extra test positions

--- MiniMaxOpening_test.py
+++ MiniMaxOpening_test.py
@@ -1,5 +1,53 @@
 from classes import Board
 
+pos = {
+        "a0" : None,
+        "a3" : "w",
+        "a6" : None,
+        "b1" : None,
+        "b3" : "w",
+        "b5" : None,
+        "c2" : None,
+        "c3" : "w",
+        "c4" : None,
+        "d4" : None,
+        "d5" : None,
+        "d6" : None,
+        "e2" : "w",
+        "e3" : "w",
+        "e4" : None,
+        "f1" : "b",
+        "f3" : "b",
+        "f5" : None,
+        "g0" : "b",
+        "g3" : "b",
+        "g6" : "b", 
+}
+
+# pos = {
+#         "a0" : None,
+#         "a3" : None,
+#         "a6" : "w",
+#         "b1" : None,
+#         "b3" : None,
+#         "b5" : "b",
+#         "c2" : None,
+#         "c3" : None,
+#         "c4" : "b",
+#         "d4" : None,
+#         "d5" : None,
+#         "d6" : None,
+#         "e2" : None,
+#         "e3" : None,
+#         "e4" : None,
+#         "f1" : None,
+#         "f3" : None,
+#         "f5" : None,
+#         "g0" : None,
+#         "g3" : None,
+#         "g6" : "w",
+# }
+
 
 # pos = {
 #         "a0" : None,
