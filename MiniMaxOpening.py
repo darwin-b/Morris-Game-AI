16f0c614f552039fe0bd3c811290a3b84a4eb656
darwin bollepalli <darwin.bollepalli@gmail.com>
2021-07-13 14:39:57 -0500

    Added comments,depth parameter & printing output

--- MiniMaxOpening.py
+++ MiniMaxOpening.py
@@ -1,86 +1,38 @@
 from classes import Board
 import sys
 
-pos = {
-        "a0" : None,
-        "a3" : "w",
-        "a6" : None,
-        "b1" : None,
-        "b3" : "w",
-        "b5" : None,
-        "c2" : None,
-        "c3" : "w",
-        "c4" : None,
-        "d4" : None,
-        "d5" : None,
-        "d6" : None,
-        "e2" : "w",
-        "e3" : "w",
-        "e4" : None,
-        "f1" : "b",
-        "f3" : "b",
-        "f5" : None,
-        "g0" : "b",
-        "g3" : "b",
-        "g6" : "b", 
-}
 
-# pos = {
-#         "a0" : None,
-#         "a3" : None,
-#         "a6" : "w",
-#         "b1" : None,
-#         "b3" : None,
-#         "b5" : "b",
-#         "c2" : None,
-#         "c3" : None,
-#         "c4" : "b",
-#         "d4" : None,
-#         "d5" : None,
-#         "d6" : None,
-#         "e2" : None,
-#         "e3" : None,
-#         "e4" : None,
-#         "f1" : None,
-#         "f3" : None,
-#         "f5" : None,
-#         "g0" : None,
-#         "g3" : None,
-#         "g6" : "w",
-# }
-
-
-def minimax_opening(pos):
+def minimax_opening(pos,depth=4):
         input_position = Board(pos)
-        print("*******************************************************")
-        print("*                   Input Position                    *")
-        print("*******************************************************")
+        print("**********************************************************************************")
+        print("*                                Input Position                                  *")
+        print("**********************************************************************************")
         print("\n")
         input_position.display_position()
-        print("*******************************************************")
-        print("*******************************************************")
+        print("************* Please wait..... computer is thinking hard to beat you *************")
 
-        color="w"
         root_position = input_position
         root_position.depth=0
 
-        depth=5
+
         levels ={}
 
         for lvl in range(0,depth+1):
             levels[lvl] = []
         
         levels[0].append(root_position)
-        temp_positions =[root_position]
+        
+        color="w"
+        # first_move_color=color
 
         for lvl in levels:
             temp = []
             _positions = []
-    
+
             if lvl %2 ==0:
                 color ="w"
             else:
-                color="b"
+                color ="b"
 
             if lvl < depth:
                 for each_board in levels[lvl]:
@@ -93,13 +45,16 @@ def minimax_opening(pos):
                         levels[lvl+1].append(child)
             
         # calculating static estimate function at leaf nodes [given depth]
+        # if first_move_color =="w":
         for board in levels[depth] :
             board.static_estimate = board.static_estimation_opening()
 
 
         for level in range(depth-1,-1,-1):
             nodes = levels[level]
+
             for node in nodes:
+                # Max strategy
                 if level % 2 ==0:
                     node.static_estimate = node.child_positions[0].static_estimate
                     node.white_move = node.child_positions[0]
@@ -109,6 +64,7 @@ def minimax_opening(pos):
                             node.static_estimate = child.static_estimate
                             node.white_move = child
 
+                # Min strategy 
                 else:
                     node.static_estimate = node.child_positions[0].static_estimate
                     node.black_move = node.child_positions[0]
@@ -117,12 +73,19 @@ def minimax_opening(pos):
                         if node.static_estimate > child.static_estimate:
                             node.static_estimate = child.static_estimate
                             node.black_move = child
-        
+
+        print("**********************************************************************************")
+        print("*                                  computer plays                                *")
+        print("**********************************************************************************")
+        print("**********************************************************************************"+"\n\n")
         root_position.white_move.display_position()
-        
+        print("Static Estimate : ",root_position.static_estimate,"\n")
+        print("**********************************************************************************")
+        print("**********************************************************************************")
+        return root_position.white_move
 
         
 
 
 
-minimax_opening(pos)
+# minimax_opening(pos)
