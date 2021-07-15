7ae9b430304381734ea8c1115a62dcfbb593e43c
darwin bollepalli <darwin.bollepalli@gmail.com>
2021-07-15 12:35:26 -0500

    release v1 (modied search algo to DFS)

--- ABOpening.py
+++ ABOpening.py
@@ -12,7 +12,6 @@ def alphabeta_opening(pos,depth=4):
         root_position = input_position
         root_position.depth=0
 
-        # depth=3
         max_min(root_position,-1000000,1000000,depth)
             
 
@@ -21,8 +20,8 @@ def alphabeta_opening(pos,depth=4):
         print("**********************************************************************************")
         print("**********************************************************************************"+"\n\n")
         root_position.white_move.display_position()
-        print("Static Estimate : ",root_position.static_estimate,"\n")
-        print("Nodes Evaluated : ",dfs(root_position,1),"\n")
+        print("Static Estimate : ",root_position.static_estimate)
+        print("Nodes Evaluated : ",dfs(root_position,c=1),"\n")
         print("**********************************************************************************")
         print("**********************************************************************************")
         return root_position.white_move
@@ -45,20 +44,16 @@ def max_min(node,a,b,depth):
         child.depth = node.depth+1
         node.child_positions.append(child)
 
-    # for child in node.child_positions:
-
         temp = min_max(child,a,b,depth)
         if v<temp :
             v=temp
             node.white_move = child
             node.static_estimate = v            
         if v>=b:
-            # node.static_estimate = v
-            # node.white_move = child
             return v
 
         a = max(v,a)
-    # node.static_estimate = v
+
     return v
 
 
@@ -80,34 +75,23 @@ def min_max(node,a,b,depth):
         child.depth = node.depth+1
         node.child_positions.append(child)
 
-
-    # for child in node.child_positions:
-
         temp = max_min(child,a,b,depth)
         if v>temp :
             v=temp
             node.black_move = child
             node.static_estimate = v  
         if v<=a:
-            # node.static_estimate = v
-            # node.black_move = child
             return v
 
         b = min(v,b)
-    # node.static_estimate = v
+
     return v    
 
 
 def dfs(node,c):
     if node.child_positions ==[]:
-        # print("============== ",c," Leaf Node================\n")
-        # node.display_position()
-        # print("================================================\n")
         return c
 
-    # print("=========== ",c," Node===============\n")
-    # node.display_position()
-    # print("=======================================\n")
     for child in node.child_positions:
         c=dfs(child,c+1)
     return c
--- MiniMaxOpening.py
+++ MiniMaxOpening.py
@@ -1,7 +1,10 @@
 from classes import Board
 import sys
 
-
+###################################################################################
+# Input:
+# Output:
+###################################################################################
 def minimax_opening(pos,depth=4):
         input_position = Board(pos)
         print("**********************************************************************************")
@@ -14,78 +17,91 @@ def minimax_opening(pos,depth=4):
         root_position = input_position
         root_position.depth=0
 
+        max_min(root_position,depth)
 
-        levels ={}
-
-        for lvl in range(0,depth+1):
-            levels[lvl] = []
         
-        levels[0].append(root_position)
-        
-        color="w"
-        # first_move_color=color
-
-        for lvl in levels:
-            temp = []
-            _positions = []
-
-            if lvl %2 ==0:
-                color ="w"
-            else:
-                color ="b"
-
-            if lvl < depth:
-                for each_board in levels[lvl]:
-                    _positions = each_board.generate_moves_opening(color=color)
-                    for _pos in _positions:
-                        child = Board(_pos)
-                        child.parent = each_board
-                        child.depth = each_board.depth+1
-                        each_board.child_positions.append(child)
-                        levels[lvl+1].append(child)
-            
-        # calculating static estimate function at leaf nodes [given depth]
-        # if first_move_color =="w":
-        for board in levels[depth] :
-            board.static_estimate = board.static_estimation_opening()
-
-
-        for level in range(depth-1,-1,-1):
-            nodes = levels[level]
-
-            for node in nodes:
-                # Max strategy
-                if level % 2 ==0:
-                    node.static_estimate = node.child_positions[0].static_estimate
-                    node.white_move = node.child_positions[0]
-
-                    for child in node.child_positions:
-                        if node.static_estimate < child.static_estimate:
-                            node.static_estimate = child.static_estimate
-                            node.white_move = child
-
-                # Min strategy 
-                else:
-                    node.static_estimate = node.child_positions[0].static_estimate
-                    node.black_move = node.child_positions[0]
-
-                    for child in node.child_positions:
-                        if node.static_estimate > child.static_estimate:
-                            node.static_estimate = child.static_estimate
-                            node.black_move = child
-
         print("**********************************************************************************")
         print("*                                  computer plays                                *")
         print("**********************************************************************************")
         print("**********************************************************************************"+"\n\n")
         root_position.white_move.display_position()
-        print("Static Estimate : ",root_position.static_estimate,"\n")
+        print("Static Estimate : ",root_position.static_estimate)
+        print("Nodes Evaluated : ",dfs(root_position,c=1),"\n")
         print("**********************************************************************************")
         print("**********************************************************************************")
         return root_position.white_move
 
         
+###################################################################################
+# Input:
+# Output:
+###################################################################################
+def max_min(node,depth):
+    if node.depth == depth:
+        node.static_estimate =  node.static_estimation_opening()
+        return node.static_estimate
+
+    v = float('-inf')
+
+    child_positions = node.generate_moves_opening(color="w")
+    for c in child_positions:
+        child = Board(c)
+        child.parent = node
+
+        if node.depth is None:
+            node.depth=0
+        child.depth = node.depth+1
+        node.child_positions.append(child)
+
+        temp = min_max(child,depth)
+        if v<temp :
+            v=temp
+            node.white_move = child
+            node.static_estimate = v
+
+    return v
 
 
+###################################################################################
+# Input:
+# Output:
+###################################################################################
+def min_max(node,depth):
+    if node.depth == depth:
+        node.static_estimate =  node.static_estimation_opening()
+        return node.static_estimate
+
+    v = float('inf')
+
+    child_positions = node.generate_moves_opening(color="b")
+    for c in child_positions:
+        child = Board(c)
+        child.parent = node
+
+        if node.depth is None:
+            node.depth=0
+        child.depth = node.depth+1
+        node.child_positions.append(child)
+
+        temp = max_min(child,depth)
+        if v>temp :
+            v=temp
+            node.black_move = child
+            node.static_estimate = v      
+
+    return v
+
+
+###################################################################################
+# Input: Node in search tree and count of nodes evaluated till given node
+# Output: Count of total nodes evaluated
+###################################################################################
+def dfs(node,c=1):
+    if node.child_positions ==[]:
+        return c
+
+    for child in node.child_positions:
+        c=dfs(child,c+1)
+    return c
 
 # minimax_opening(pos)
